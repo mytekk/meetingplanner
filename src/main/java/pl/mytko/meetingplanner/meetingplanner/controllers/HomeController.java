14e90b6aa75c623bc74cda30c0e96a90ed2c774a
Bartłomiej Mytko <bartlomiej.mytko@gmail.com>
2017-10-26 22:11:58 +0200

    Created HomeController and added simple endpoint for '/' and '/home' paths

---
+++ src/main/java/pl/mytko/meetingplanner/meetingplanner/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package pl.mytko.meetingplanner.meetingplanner.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @GetMapping(path = {"/", "/home"})
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView("home");
+        return modelAndView;
+    }
+}
